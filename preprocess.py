79a8c52dc4bf2201c652cecfc1e4de049bcda1ab
netta.savin <nettasav@gmail.com>
2025-05-25 15:33:36 +0300

    save the users and movies encoding to a pickle file

--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,21 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+import pickle
 
 
 def split_data(df):
+
     user_encoder = LabelEncoder()
     movie_encoder = LabelEncoder()
     df["userId"] = user_encoder.fit_transform(df["userId"])
     df["movieId"] = movie_encoder.fit_transform(df["movieId"])
 
-    num_users = df["userId"].nunique()
-    num_movies = df["movieId"].nunique()
+    file_path = f"/opt/models"
+    with open(f"{file_path}/user_encoder.pkl", "wb") as f:
+        pickle.dump(user_encoder, f)
+
+    with open(f"{file_path}/movie_encoder.pkl", "wb") as f:
+        pickle.dump(movie_encoder, f)
 
     unique_users = df["userId"].unique()
     train_users, test_users = train_test_split(
